17317cfcd106efe14e982c89cd8d53d7eb9b48d8
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-12-28 01:27:25 +0000

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://marlonlavanda.github.io/',
+  base: 'https://marlonlavanda.github.io/gh-react-vite/',
   plugins: [react()]
 })
